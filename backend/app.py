ea01a05d022d60d461f258f57087853a1f2d53e1
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2025-02-08 15:14:42 +0100

    Add saving of RSVPs in a database in the backend

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
+db = SQLAlchemy(app)
 CORS(app)  # Enable Cross-Origin Resource Sharing
+
+class RSVP(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    attending = db.Column(db.Boolean, nullable=False)
 
 @app.route('/rsvp', methods=['POST'])
 def rsvp():
@@ -12,9 +21,12 @@ def rsvp():
     name = data.get('name')
     email = data.get('email')
     attending = data.get('attending')
-    # Here you can save the data to a database or perform other actions
-    print(f'RSVP received: {name}, {email}, {attending}')
+    new_rsvp = RSVP(name=name, email=email, attending=attending)
+    db.session.add(new_rsvp)
+    db.session.commit()
     return jsonify({'message': 'RSVP received'}), 200
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=5000)
