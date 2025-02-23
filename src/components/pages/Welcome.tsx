99a01523b5c7167d5b4f7a282777ebd518da02fd
Sofia Jacobsson <sofiajacobsson95@gmail.com>
2025-02-23 18:46:27 +0100

    Adds styling of main page to make it look like the sketches

--- src/components/pages/Welcome.tsx
+++ src/components/pages/Welcome.tsx
@@ -3,13 +3,22 @@ import Button from "../Button";
 
 export const Welcome = () => {
   return (
-    <div>
-      <div className="content">
-        {/* <hr /> */}
+    <div className="pageContent">
+      <div className="headingsContainer">
+        <h2 className="fancyText h2">Välkommen på bröllop</h2>
+        <div className="fancyText">16 - 17 augusti 2025</div>
         <div className="fancyText">Arholma</div>
-        <div className="">16 - 17 augusti 2025</div>
-        <Button title="o.s.a." />
       </div>
+      <p className="">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <Button title="o.s.a." />
     </div>
   );
 };
